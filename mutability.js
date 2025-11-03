8bab15c7d73cc742cbd23bd24a33881aad5065cc
nallain <nathan240304@gmail.com>
2025-11-03 13:11:18 +0100

    mutability 3

--- mutability.js
+++ mutability.js
@@ -1,3 +1,17 @@
+const person = {
+  name: 'Rick',
+  age: 77,
+  country: 'US',
+}
+
 const samePerson = person
-const clone1 = Object.freeze(person)
-const clone2 = Object.freeze(person)
+samePerson.age += 1
+samePerson.country = 'FR'
+const clone1 = person
+clone1.age += 1
+clone1.country = 'FR'
+Object.freeze(clone1)
+const clone2 = person
+clone2.age += 1
+clone2.country = 'FR'
+Object.freeze(clone2)
