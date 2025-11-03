62fd878244bf810014b8c4bdda162dc2679fadd8
nallain <nathan240304@gmail.com>
2025-11-03 12:45:09 +0100

    declaration 6

--- declarations.js
+++ declarations.js
@@ -4,9 +4,10 @@ const obj = {str: "yoyo", num : 17, bool : true, undef : undefined}
 const nested = {arr : [4, undefined, '2'], obj : {str : "test", num : 7, bool : false}}
 
 Object.freeze(nested)
+Object.freeze(nested.obj)
 Object.freeze(arr)
 Object.freeze(obj)
-
+Object.deepFreeze()
 console.log(escapeStr)
 
 //`\/"'
