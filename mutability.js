09f766d9a4badf5e201792a7520b782917eaa838
nallain <nathan240304@gmail.com>
2025-11-03 13:16:35 +0100

    mutability 4

--- mutability.js
+++ mutability.js
@@ -2,16 +2,19 @@ const person = {
   name: 'Rick',
   age: 77,
   country: 'US',
-}
+};
 
-const samePerson = person
-samePerson.age += 1
-samePerson.country = 'FR'
-const clone1 = person
-clone1.age += 1
-clone1.country = 'FR'
-Object.freeze(clone1)
-const clone2 = person
-clone2.age += 1
-clone2.country = 'FR'
-Object.freeze(clone2)
+// Create shallow copies using spread syntax
+const samePerson = { ...person }; // true clone
+samePerson.age += 1;
+samePerson.country = 'FR';
+
+const clone1 = { ...person };
+clone1.age += 1;
+clone1.country = 'FR';
+Object.freeze(clone1);
+
+const clone2 = { ...person };
+clone2.age += 1;
+clone2.country = 'FR';
+Object.freeze(clone2);
