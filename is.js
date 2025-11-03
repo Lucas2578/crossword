61aefbec819ee60db1fac193ff5db4aa53fb5d98
nallain <nathan240304@gmail.com>
2025-11-03 17:18:20 +0100

    is

---
+++ is.js
@@ -0,0 +1,14 @@
+const is = {};
+
+is.num = value => typeof value === 'number' && !Number.isNaN(value);
+is.nan = value => Number.isNaN(value);
+is.str = value => typeof value === 'string';
+is.bool = value => typeof value === 'boolean';
+is.undef = value => typeof value === 'undefined';
+is.def = value => typeof value !== 'undefined';
+is.arr = value => Array.isArray(value);
+is.obj = value => value === null || (typeof value === 'object' && !Array.isArray(value));
+is.fun = value => typeof value === 'function';
+is.truthy = value => Boolean(value);
+is.falsy = value => !value;
+
