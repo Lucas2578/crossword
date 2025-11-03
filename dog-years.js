c8d61e2345c6439056ba48dcaa901ec79a79cd44
nallain <nathan240304@gmail.com>
2025-11-03 15:49:25 +0100

    dog-years 3

--- dog-years.js
+++ dog-years.js
@@ -1,15 +1,15 @@
-const planet = {
-        earth : 1,
-        mercury : 0.2408467,
-        venus :  0.61519726,
-        mars : 1.8808158,
-        jupiter : 11.862615,
-        saturn : 29.447498,
-        uranus : 84.016846,
-        neptune : 164.79132
+    const planet = {
+            earth : 1,
+            mercury : 0.2408467,
+            venus :  0.61519726,
+            mars : 1.8808158,
+            jupiter : 11.862615,
+            saturn : 29.447498,
+            uranus : 84.016846,
+            neptune : 164.79132
+        }
+    function dogYears(Planet, second){
+        return Math.round(second/31557600 * 7 / planet[Planet]*100)/100
     }
-function dogYears(Planet, second){
-    return Math.round(second/31557600 * 7 * planet[Planet]*100)/100
-}
 
-//console.log(Math.round(dogYears('earth', 1000000000)*100)/100)
+    console.log(dogYears('earth', 1000000000))
