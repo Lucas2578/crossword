5bd9ee2d606cff7e407cb591600c9d7d96301cf0
nallain <nathan240304@gmail.com>
2025-11-03 12:34:16 +0100

    declaration 5

--- declarations.js
+++ declarations.js
@@ -1,7 +1,7 @@
 const escapeStr = /*"'\\/\"\'"*/ "\`\\/\"'"
 const arr = [4, '2']
 const obj = {str: "yoyo", num : 17, bool : true, undef : undefined}
-const nested = {arr : [4, undefined, '2'], obj : {str : "test", num : "7", bool : false}}
+const nested = {arr : [4, undefined, '2'], obj : {str : "test", num : 7, bool : false}}
 
 Object.freeze(nested)
 Object.freeze(arr)
