9768c85c6245be0957b7c3e12847ff6c57fe6505
nallain <nathan240304@gmail.com>
2025-11-03 13:39:41 +0100

    last-first-kiss 1

---
+++ last-first-kiss.js
@@ -0,0 +1,10 @@
+function first(array){
+    return array[0]
+}
+function last(array){
+    return array[array.length - 1]
+}
+function kiss(array){
+    const resurlt = [array[0], array[array.length - 1]]
+    return resurlt
+}
