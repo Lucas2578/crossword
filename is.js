89c897a2331dc3aa5a156d84910618ab10cf3f90
nallain <nathan240304@gmail.com>
2025-11-03 17:24:06 +0100

    is 3

--- is.js
+++ is.js
@@ -7,7 +7,7 @@ is.bool = value => typeof value === 'boolean';
 is.undef = value => typeof value === 'undefined';
 is.def = value => typeof value !== 'undefined';
 is.arr = value => Array.isArray(value);
-is.obj = value => value === null || (typeof value === 'object' && !Array.isArray(value));
+is.obj = value => value === null || (typeof value === 'object');
 is.fun = value => typeof value === 'function';
 is.truthy = value => Boolean(value);
 is.falsy = value => !value;
