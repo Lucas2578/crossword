feac8923e74e73a719aa65bde170f77e2bf6d58f
nallain <nathan240304@gmail.com>
2025-11-03 16:41:08 +0100

    method-man

---
+++ method-man.js
@@ -0,0 +1,19 @@
+function words (str){
+    return str.split(" ")
+}
+function sentence(arrStr){
+    return arrStr.join(" ")
+}
+function yell(str){
+    return str.toUpperCase  
+}
+
+function whisper(str){
+    return '*'+str.toLowerCase + '*'
+}
+function capitalize(str){
+    str = str.toLowerCase
+    let arr = str.split("")
+    arr[0] = arr[0].toUpperCase
+    return arr.join("")
+}
