fe8015aea3bd49d4fc70bb02f6f23ed6cc7c0c62
nallain <nathan240304@gmail.com>
2025-11-04 12:43:08 +0100

    collection 4

--- collections.js
+++ collections.js
@@ -19,8 +19,8 @@ function strToSet (str) {
 function mapToObj (map) {
     return Object.fromEntries(map)
 }
-function objToArr (obj) {
-    return Object.entries(obj)
+function objToArr(obj) {
+  return Object.values(obj);
 }
 function objToMap (obj) {
     return new Map(Object.entries(obj))
