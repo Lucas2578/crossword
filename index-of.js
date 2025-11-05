1436b6b836bd92b15262a966e0ea07c7f48dff12
nallain <nathan240304@gmail.com>
2025-11-05 13:24:04 +0100

    index 4

--- index-of.js
+++ index-of.js
@@ -9,17 +9,14 @@ function indexOf (arr, val, start = 0){
     return -1
 }
 
-function lastIndexOf (arr, val, start = arr.length-1){
-    let i = start
-    while (i>0){
-        if (arr[i] === val){
-            return i
-        }
-        i--
+function lastIndexOf(arr, val, start = arr.length - 1) {
+    let i = start;
+    while (i >= 0) {
+        if (arr[i] === val) return i;
+        i--;
     }
-    return -1
+    return -1;
 }
-
 function includes (arr, val , start = 0){
     let i = start
     while (i< arr.length){
