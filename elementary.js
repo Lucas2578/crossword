07f90da1e6e3502ffa65cef2cec1c61c4584450f
nallain <nathan240304@gmail.com>
2025-11-05 12:46:02 +0100

    elementary 2)

--- elementary.js
+++ elementary.js
@@ -1,33 +1,35 @@
-function multiply (a,b) {
-    let result = 0
-    let i =0
-    while (i<a){
-        result += b
-        i++
+function multiply (a, b) {
+    let result = 0;
+    let i = 0;
+
+    while (i < a) {
+        result += b;
+        i++;
     }
-    return result
+    return result;
 }
 
-function divide (a,b) {
-    let result = a
-    let i =0
-    while (result>b){
-        result -= b
-        i++
+function divide (a, b) {
+    let result = a;
+    let i = 0;
+
+    while (result >= b) {
+        result -= b;
+        i++;
     }
-    return i
+    return i;
 }
 
-function modulo (a,b) {
-    let result = a
-    let i =0
-    while (result>b){
-        result -= b
-        i++
+function modulo (a, b) {
+    let result = a;
+
+    while (result >= b) {
+        result -= b;
     }
-    return i/2*10
+    return result; // ✅ le reste
 }
+
 
 console.log(multiply(3,3))
 console.log(divide(10,3))
-console.log(modulo(3,3))
+console.log(modulo(10,3))
