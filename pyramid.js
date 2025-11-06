036597895bed387201eab64d46e28390cfb1e662
nallain <nathan240304@gmail.com>
2025-11-06 17:26:20 +0100

    pyramide

---
+++ pyramid.js
@@ -0,0 +1,30 @@
+function pyramid (str, num){
+    let i = 1
+    let y = 0
+    let nbrSpace = num-1
+    let result = ""
+    let repeat = 1
+
+    while(i<num+1){
+
+        let x = nbrSpace
+        while(x>0){
+            result+= " "
+            x--
+        }
+        while(y<repeat){
+            result += str
+            y++
+        }
+        if (i !== num){
+            result += "\n"
+        }
+        i++
+        y = 0
+        nbrSpace--
+        repeat = repeat +2
+    }
+    return result
+}
+
+console.log(pyramid("*",5))
