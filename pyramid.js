f627fd0b0d065a28face2328b28b7961c49efb49
nallain <nathan240304@gmail.com>
2025-11-06 17:50:03 +0100

    pyramide 8

--- pyramid.js
+++ pyramid.js
@@ -1,12 +1,12 @@
-function pyramid (str, num){
+function pyramid(str, num){
   let result = "";
   let repeat = 1;
-  let nbrSpace = num * 2 - 2; // more initial spacing for centered alignment
+  let nbrSpace = (num - 1) * str.length; // scale spaces by length of str
 
   for (let i = 0; i < num; i++) {
     result += " ".repeat(nbrSpace) + str.repeat(repeat);
     if (i !== num - 1) result += "\n";
-    nbrSpace -= 2;
+    nbrSpace -= str.length;
     repeat += 2;
   }
   return result;
