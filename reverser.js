89eb29cb2578e6469ce79e0071026b678bc2b609
nallain <nathan240304@gmail.com>
2025-11-06 12:50:33 +0100

    reverser 4

--- reverser.js
+++ reverser.js
@@ -1,19 +1,34 @@
-function reverse (array) {
-    let i = array.length -1
-    if (Array.isArray(value)){
-        let final = []
-        while(i>=0){
-            final.push(array[i])
-            i--
-         }
-    }else {
-        let final = ""
-        while(i>=0){
-            final += array[i]
-            i--
-        }
-    }
-    return final
-}
+// function reverse (array) {
+//     let i = array.length -1
+//     if (Array.isArray(array)){
+//         let final = []
+//         while(i>=0){
+//             final.push(array[i])
+//             i--
+//          }
+//     }else {
+//         let final = ""
+//         while(i>=0){
+//             final += array[i]
+//             i--
+//         }
+//     }
+//     return final
+// }
 
 console.log(reverse(["banana", "apple", "peach"]))
+console.log(reverse("peach"))
+
+function reverse (array){
+    let i = array.length -1
+    let final = []
+    while(i>=0){
+        final.push(array[i])
+        i--
+    }
+    if (Array.isArray(array)){
+        return final 
+    }else{
+        return final.join("")
+    }
+}
