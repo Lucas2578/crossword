6ef5144cc16661a6ae80d022cbc16badfb086214
nallain <nathan240304@gmail.com>
2025-11-07 13:57:13 +0100

    same-amount 2

--- same-amount.js
+++ same-amount.js
@@ -1,9 +1,10 @@
-function sameAmount (str, regex1, regex2){
-    let test1 = regex1.test(str)
-    let test2 = regex2.test(str)
-    if (test1 && test2){
-        return true 
-    }else {
-        return false
-    }
+function sameAmount(str, regex1, regex2) {
+  // make sure we get *all* matches by adding the global flag
+  const r1 = new RegExp(regex1.source, regex1.flags.includes('g') ? regex1.flags : regex1.flags + 'g');
+  const r2 = new RegExp(regex2.source, regex2.flags.includes('g') ? regex2.flags : regex2.flags + 'g');
+
+  const matches1 = str.match(r1) || [];
+  const matches2 = str.match(r2) || [];
+
+  return matches1.length === matches2.length;
 }
