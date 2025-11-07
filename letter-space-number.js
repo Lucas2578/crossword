c2c48f851306ef2a83718c802c7134edd57dd06a
nallain <nathan240304@gmail.com>
2025-11-07 13:18:51 +0100

    letterspacenumber 5

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,6 +1,13 @@
 function letterSpaceNumber (str){
+    let final = []
     const test = /[a-zA-Z]\s\d(?![a-zA-Z0-9])/g;  
-    return str.match(test)
+    final = str.match(test)
+    if (final != null){
+        return final
+    }else {
+        return []
+    }
 }
 
 console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.'))
+console.log(letterSpaceNumber('I like 7up.'))
