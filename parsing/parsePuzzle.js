1b3493b9fc6460fb5f0c56363ac42b0337cef2e3
Lucas2578 <lucasdunis@gmail.com>
2025-11-07 19:46:58 +0100

    Add comment
    Add new comment on parsePuzzle function

--- parsing/parsePuzzle.js
+++ parsing/parsePuzzle.js
@@ -1,3 +1,13 @@
+/**
+ * Parses a puzzle string into an array of lines
+ * Splits the puzzle by newline characters to create individual rows
+ * @param {string} Puzzle - Multi-line puzzle string with '\n' separators
+ * @returns {string[]} Array of strings, each representing a puzzle line
+ * @example
+ * // Input: "12.\n0.0\n..."
+ * // Output: ["12.", "0.0", "..."]
+*/
+
 function parsePuzzle (Puzzle){
     let tableauLigne = Puzzle.split('\n')
     return tableauLigne
